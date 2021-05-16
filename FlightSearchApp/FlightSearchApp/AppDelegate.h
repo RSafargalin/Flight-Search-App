56459bc063a1a0e335501ca3754d0821f8f45728
Ruslan Safargalin <r.safargalin@icloud.com>
2021-05-16 13:38:07 +0500

    Creating a basic structure
    Реализованы базовые классы для работы с данными

---
+++ FlightSearchApp/FlightSearchApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FlightSearchApp
+//
+//  Created by Ruslan Safargalin on 16.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FlightSearchApp/FlightSearchApp/MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  FlightSearchApp
+//
+//  Created by Ruslan Safargalin on 16.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
